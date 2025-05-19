0eed505e3984b8681d7808ef4bab6c34cf65a282
Marc <mreta0002@gmail.com>
2025-05-19 14:10:43 -0700

    set up for FastAPI

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+# Necessary Imports
+from fastapi import FastAPI                   # The main FastAPI import
+from fastapi.responses import HTMLResponse    # Used for returning HTML responses
+from fastapi.staticfiles import StaticFiles   # Used for serving static files
+from fastapi.responses import JSONResponse
+import uvicorn                                # Used for running the app
+
+# Configuration
+app = FastAPI()                   # Specify the "app" that will run the routing
+
+# Mount the static directory
+app.mount("/static", StaticFiles(directory="backend/static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def get_index_html() -> HTMLResponse:
+    with open("frontend/index.html") as html:
+        return HTMLResponse(content=html.read())
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=6543)
